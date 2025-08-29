f696a8cd8faaf9c485964bc217bb952fd0f047a6
Shashiranjan singh <singhshashiranjan34@gmail.com>
2025-08-29 11:43:04 +0530

    Fix formatting issues in signup page

--- next js try/my-app/app/signup/page.tsx
+++ next js try/my-app/app/signup/page.tsx
@@ -1,7 +1,5 @@
 "use client"
-
 import axios from "axios"
-
 export default function Signup() {
     return <div className="w-screen h-screen flex justify-center intems-center">
         <div className="border p-2">
@@ -11,7 +9,6 @@ export default function Signup() {
             <br />
             <button onClick={() => {
                 axios.post("http://localhost:3000/api/v1/signup")
-
             }}>Sign up</button>
         </div>
     </div>
